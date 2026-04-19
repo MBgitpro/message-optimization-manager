299c5af87b12d71edb9eb3ab4b9ad676beaad50f
MBgitpro <myles.boyda@gmail.com>
2026-04-19 17:14:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import Toaster  from "./components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
